445b3780dc42982fedcb6c606a4e05f9f9048b0b
Zaky Putra Kresya <zakyputrakresya@gmail.com>
2023-09-10 15:15:21 +0700

    update

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const cors = require('cors'); // Import middleware CORS
 const port = 2900; // Port
 const path = require('path');
 const pool = require("./db");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads/"); // Lokasi penyimpanan file
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext); // Nama file akan menjadi timestamp saat diunggah
+  },
+});
+
+const upload = multer({ storage });
 
 
 app.use(cors())
@@ -44,6 +57,37 @@ app.get("/dashboard", async (req, res) => {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
+});
+
+app.post("/dashboard", upload.single("file"), (req, res) => {
+  // if (!req.file) {
+  //   return res.status(400).json({ error: "Tidak ada file yang diunggah" });
+  // }
+
+  // Dapatkan data 'author' dari FormData
+  const title = req.body.title;
+  const description = req.body.description;
+  const filename = "/uploads/" + req.file.filename; // Nama file yang diunggah
+
+  // Update data di tabel app_slideshow
+  const query = `
+      UPDATE app_welcome_title
+      SET title = $1,
+          description = $2,
+      WHERE id = 1
+    `;
+
+  // Ganti $1, $2, dan $3 dengan nilai yang sesuai
+  pool.query(query, [title, description], (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: "Gagal melakukan update data " });
+    }
+
+    // Jika query update berhasil, kirim respons sukses
+    res.json({
+      message: "File berhasil diunggah dan informasi diupdate",
+    });
+  });
 });
 app.use((req, res, next) => {
   res.status(500).json({ message: 'Route not found'})
