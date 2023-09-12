05acb4d8a529b08a9e6ea465478e61461b440eb3
Zaky Putra Kresya <zakyputrakresya@gmail.com>
2023-09-12 16:20:58 +0700

    upt

--- db.js
+++ db.js
@@ -3,11 +3,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: '85.31.225.77',
-  port: 5433,
-  database: 'postgres',
-  password: 'sarirotib451',
+  user: 'uv7psmzz0ikflqglagh9',
+  host: 'bksnmpkwrteg7lwulccx-postgresql.services.clever-cloud.com',
+  database: 'bksnmpkwrteg7lwulccx',
+  password: 'xeemT9PDaaHGfXjKB6pgC92YSC0BGk',
 });
 
 module.exports = pool;
