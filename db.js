5bc6959d7cef7dc8d4255d1a3661555927513cc0
Zaky Putra Kresya <zakyputrakresya@gmail.com>
2023-09-12 15:25:46 +0700

    update

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: '85.31.225.77',
+  port: 5433,
   database: 'postgres',
   password: 'sarirotib451',
 });
