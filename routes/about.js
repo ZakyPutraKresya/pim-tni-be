125470cef2ac71ba966cd29b314671170caee11b
Zaky Putra Kresya <zakyputrakresya@gmail.com>
2023-09-13 20:35:37 +0700

    update

--- routes/about.js
+++ routes/about.js
@@ -42,5 +42,107 @@ router.get("/foreword", async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+router.post("/mission", (req, res) => {
+  const { title, description } = req.body;
+
+  const query = `UPDATE app_mission_about
+    SET title = $1,
+        description = $2
+    WHERE id = 1`;
+  pool.query(
+    query,
+    [title, description],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: "Gagal melakukan update data " });
+      }
+      // if success return response
+      res.status(200).json({
+        message: "Successfully updated data",
+      });
+    }
+  );
+});
+router.get("/mission", async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM app_mission_about");
+    const data = users.rows[0];
+    res.status(200).json([{
+        title: data.title,
+        description: data.description
+    }]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+router.post("/vision", (req, res) => {
+  const { title, description } = req.body;
+
+  const query = `UPDATE app_vision_about
+    SET title = $1,
+        description = $2
+    WHERE id = 1`;
+  pool.query(
+    query,
+    [title, description],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: "Gagal melakukan update data " });
+      }
+      // if success return response
+      res.status(200).json({
+        message: "Successfully updated data",
+      });
+    }
+  );
+});
+router.get("/vision", async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM app_vision_about");
+    const data = users.rows[0];
+    res.status(200).json([{
+        title: data.title,
+        description: data.description
+    }]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+router.post("/description", (req, res) => {
+  const { title, description } = req.body;
+
+  const query = `UPDATE app_description_about
+    SET title = $1,
+        description = $2
+    WHERE id = 1`;
+  pool.query(
+    query,
+    [title, description],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: "Gagal melakukan update data " });
+      }
+      // if success return response
+      res.status(200).json({
+        message: "Successfully updated data",
+      });
+    }
+  );
+});
+router.get("/description", async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM app_description_about");
+    const data = users.rows[0];
+    res.status(200).json([{
+        title: data.title,
+        description: data.description
+    }]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
